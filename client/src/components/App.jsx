d4d0148fabc8f7a0cd8bb20882ec3e118309459f
Nate Foster <nate.s.foster@gmail.com>
2023-04-19 19:41:04 -1000

    replace TodoList header with Header component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
+import Header from './Header.jsx';
 import TodoList from './TodoList.jsx';
 
 export default function App() {
   const [items, setItems] = useState(["item1", "item2", "item3"]);
 
   return (
-    <TodoList items={items} />
+    <div className="app">
+      <Header />
+      <TodoList items={items} />
+    </div>
   );
 }
--- client/src/components/TodoList.jsx
+++ client/src/components/TodoList.jsx
@@ -4,9 +4,6 @@ import Item from './Item.jsx';
 export default function TodoList({items}) {
   return (
     <div className="todoList">
-      <div className="todoListHeader">
-        <h3>Todo List</h3>
-      </div>
       {items.map((item, i) => <Item item={item} key={i} />)}
     </div>
   );
